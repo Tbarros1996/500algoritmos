82e93a07d2428d54a2fb21eb5c6e23fea0f73107
Thiago Barros <86922706+Tbarros1996@users.noreply.github.com>
2024-03-03 20:59:26 -0300

    Capitulo_4

---
+++ Capitulo_4/algoritmo_241.py
@@ -0,0 +1,13 @@
+# 500 Exercícios Resolvidos com Python
+# Thiago Barros
+# Exercícios resolvidos com base no Livro - 500 Algoritimos Resolvidos (ANITA LOPES E GUTO GARCIA)
+# Algoritimo Numero 241
+# Capitulo 4
+
+"""
+Criar um algoritmo que imprima a tabuada de multiplicar do número 3
+
+"""
+
+for i in range(1,11):
+    print(f'3 X {i} = {3*i}')
